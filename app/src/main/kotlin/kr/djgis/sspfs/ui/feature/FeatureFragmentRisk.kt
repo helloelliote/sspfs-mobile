763e33594b9db42861100942e22355977fd2d3f5
helloelliote <hello.elliote@gmail.com>
2022-06-09 02:31:05 +0900

    feature: 현장조사, 후보지평가, 위험도평가, 시설구분평가 입력폼

---
+++ app/src/main/kotlin/kr/djgis/sspfs/ui/feature/FeatureFragmentRisk.kt
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2022 대진기술정보(주) All Rights Reserved
+ */
+
+package kr.djgis.sspfs.ui.feature
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import kr.djgis.sspfs.databinding.FragmentFeatureRiskBinding
+import kr.djgis.sspfs.model.FeatureVMFactory
+import kr.djgis.sspfs.model.FeatureViewModel
+
+class FeatureFragmentRisk : Fragment() {
+
+    private val viewModel: FeatureViewModel by activityViewModels { FeatureVMFactory }
+
+    // This property is only valid between onCreateView and onDestroyView.
+    private var _binding: FragmentFeatureRiskBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?,
+    ): View {
+        _binding = FragmentFeatureRiskBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = this@FeatureFragmentRisk.viewModel
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
