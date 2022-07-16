c6d4ec6a6b9fd1c4c66a47ae75965a6ec4487755
helloelliote <hello.elliote@gmail.com>
2022-07-16 21:01:30 +0900

    feature: Update #toString() for Feature class

--- app/src/main/kotlin/kr/djgis/sspfs/data/FeatureB.kt
+++ app/src/main/kotlin/kr/djgis/sspfs/data/FeatureB.kt
@@ -102,7 +102,9 @@ class FeatureB(
     var eva_opim: String?,
     var eva_opiu: String?,
 ) : Feature() {
-
+    override fun toString(): String {
+        return "FeatureB(cat_funl=$cat_funl, cat_funm=$cat_funm, cat_funu=$cat_funu, cat_impl=$cat_impl, cat_impm=$cat_impm, cat_impu=$cat_impu, fac_len=$fac_len, fac_wid=$fac_wid, riv_widl=$riv_widl, riv_widm=$riv_widm, riv_widu=$riv_widu, bnk_hgtl=$bnk_hgtl, bnk_hgtm=$bnk_hgtm, bnk_hgtu=$bnk_hgtu, crn_wdll=$crn_wdll, crn_wdlm=$crn_wdlm, crn_wdlu=$crn_wdlu, crn_wdrl=$crn_wdrl, crn_wdrm=$crn_wdrm, crn_wdru=$crn_wdru, dmg_lifl=$dmg_lifl, dmg_lifm=$dmg_lifm, dmg_lifu=$dmg_lifu, dmg_prol=$dmg_prol, dmg_prom=$dmg_prom, dmg_prou=$dmg_prou, dmg_txtl=$dmg_txtl, dmg_txtm=$dmg_txtm, dmg_txtu=$dmg_txtu, fld_cdel=$fld_cdel, fld_cdem=$fld_cdem, fld_cdeu=$fld_cdeu, fld_txtl=$fld_txtl, fld_txtm=$fld_txtm, fld_txtu=$fld_txtu, bnk_stll=$bnk_stll, bnk_stlm=$bnk_stlm, bnk_stlu=$bnk_stlu, bnk_strl=$bnk_strl, bnk_strm=$bnk_strm, bnk_stru=$bnk_stru, rev_mall=$rev_mall, rev_malm=$rev_malm, rev_malu=$rev_malu, rev_marl=$rev_marl, rev_marm=$rev_marm, rev_maru=$rev_maru, rev_stll=$rev_stll, rev_stlm=$rev_stlm, rev_stlu=$rev_stlu, rev_strl=$rev_strl, rev_strm=$rev_strm, rev_stru=$rev_stru, dor_pasl=$dor_pasl, dor_pasm=$dor_pasm, dor_pasu=$dor_pasu, riv_chgl=$riv_chgl, riv_chgm=$riv_chgm, riv_chgu=$riv_chgu, riv_depl=$riv_depl, riv_depm=$riv_depm, riv_depu=$riv_depu, riv_pltl=$riv_pltl, riv_pltm=$riv_pltm, riv_pltu=$riv_pltu, riv_posl=$riv_posl, riv_posm=$riv_posm, riv_posu=$riv_posu, riv_dmgl=$riv_dmgl, riv_dmgm=$riv_dmgm, riv_dmgu=$riv_dmgu, eva_prxl=$eva_prxl, eva_prxm=$eva_prxm, eva_prxu=$eva_prxu, eva_infl=$eva_infl, eva_infm=$eva_infm, eva_infu=$eva_infu, eva_opel=$eva_opel, eva_opem=$eva_opem, eva_opeu=$eva_opeu, eva_opil=$eva_opil, eva_opim=$eva_opim, eva_opiu=$eva_opiu) ${super.toString()}"
+    }
 }
 
 object FeatureBDiffCallback : DiffUtil.ItemCallback<FeatureB>() {
